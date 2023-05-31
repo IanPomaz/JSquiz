46f0772125d46053f09b7c4d8f8d2541fa8bc9af
IanPomaz <128009980+IanPomaz@users.noreply.github.com>
2023-05-30 21:08:55 -0400

    added varibales and started the startQuiz function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,40 @@
+var questions = [ 
+    {
+        question: "What is the main purpose for Javascript?",
+        choices: ["Structure", "Styling", "Functionality", "All of the above"],
+        answer: 2,
+    },
+    {
+        question: "q",
+        choices: ["a"],
+        answer: 0,
+    },
+    {
+        question: "q",
+        choices: ["a"],
+        answer: 0,
+    },
+    {
+        question: "q",
+        choices: ["a"],
+        answer: 0,
+    },
 
+]
+
+var questionIndex = 0;
+var timer = 60;
+var timerInterval;
+var score = 0;
+var startButton = document.getElementById("start-button");
+var questionBox = document.getElementById("question-box");
+var question = document.getElementById("question");
+var choices = document.getElementById("choices");
+var timer = document.getElementById("timer");
+var scoreForm = document.getElementById("score");
+
+addEventListener("click", startQuiz)
+
+function startQuiz() {
+    element.remove()
+}
