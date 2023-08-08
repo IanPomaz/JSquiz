c03c5a0fe349ee6b624a7eb93518a7227cbffac4
IanPomaz <128009980+IanPomaz@users.noreply.github.com>
2023-08-07 23:18:08 -0400

    timer function and show question

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,60 +1,66 @@
-// var questions = [ 
-//     {
-//         question1: "What is the main purpose for Javascript?",
-//         choices1: ["Structure", "Styling", "Functionality", "All of the above"],
-//         answer1: 2,
-//     },
-//     {
-//         question2: "q",
-//         choices2: ["a"],
-//         answer2: 0,
-//     },
-//     {
-//         question3: "q",
-//         choices3: ["a"],
-//         answer3: 0,
-//     },
-//     {
-//         question4: "q",
-//         choices4: ["a"],
-//         answer4: 0,
-//     },
+let secondsLeft = 60;
+let questionCount = 0;
+let score = document.getElementById('highscores');
+let timerDisplay = document.getElementById('time');
+const startButton = document.getElementById('start-button');
 
-// ]
+const questions = [ 
+    {
+        Q: 'Quenstions 1',
+        A: ['a','b','c','d'],
+        C: 'b'
+    },
+    {
+        Q: 'Quenstions 2',
+        A: ['a','b','c','d'],
+        C: 'b'
+    },
+    {
+        Q: 'Quenstions 3',
+        A: ['a','b','c','d'],
+        C: 'b'
+    },
+    {
+        Q: 'Quenstions 4',
+        A: ['a','b','c','d'],
+        C: 'b'
+    },
+    {
+        Q: 'Quenstions 5',
+        A: ['a','b','c','d'],
+        C: 'b'
+    }
+]
 
-var questionIndex = 0;
-var timer = 60;
-var timerInterval;
-var score = 0;
-var startButton = document.getElementById("start-button");
-var questionBox = document.getElementById("question-box");
-var question = document.getElementById("question");
-var choices = document.getElementById("choices");
-var timer = document.getElementById("timer");
-var scoreForm = document.getElementById("score");
 
-addEventListener("click", startQuiz)
+function timer() {
+    const timerInterval = setInterval(function () {
+        secondsLeft--;
+        timerDisplay.textContent = secondsLeft;
+      
+        if (secondsLeft === 0 || questionCount === questions.length) {
+            clearInterval(timerInterval);
+        }
+    }, 1000);
+}
+
+startButton.addEventListener('click', () => {
+
+    timer(); 
+    startQuiz();
+})
 
 function startQuiz() {
-    //remove the start button
-    // startButton.classList.add("hide");
-    //remove the start element
-    // var start = document.getElementById("start").remove();
-    //add the question
-    document.getElementById("question").innerHTML = questions.question1;
-    //add the answer
-    //make the answers clickable
-    //assign the correct answer
+    showQuestion();
+
 }
+
+
 function showQuestion() {
-        //add the question
-        document.getElementById("question").innerHTML = questions.question1;
-        console.log(questions.question1)
-        //add the answer
-        //make the answers clickable
-        //assign the correct answer
+    const questionBox = document.getElementById('questions')
+    const currentQuestion = questions[questionCount]
 }
-showQuestion()
+
 
 // function checkAnswer {
 
